8a9091e67d0146f5a2d51bc709161032e35212d8
Avi <avishekuser@gmai.com>
2023-04-01 15:53:00 +0600

    card updated

--- src/components/Blog-information/Bloginfo.jsx
+++ src/components/Blog-information/Bloginfo.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './Bloginfo.css'
 
 const Bloginfo = (props) => {
-    const {cover_picture,author_picture,name,title,date,time} = props.blogInfo
+    const {cover_picture,author_picture,author_name,title,date,time} = props.blogInfo
     return (
         <div className='blogInfo'>
             <img src={cover_picture} alt="" />
             <div className='author-info'>
                 <img src={author_picture} alt="" />
-                <div>
-                <p>{date}</p>
-                <p>{time} min read</p>
+                <div className=''>
+                <p>{author_name}</p>
+                <p> {date}</p> 
                 </div>
-                
+                <div>
+                <p>{time} min read</p>
+                <button>aaa</button> 
+                </div>    
             </div>
         </div>
     );
